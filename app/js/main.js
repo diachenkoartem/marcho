eb88a09bcb4350dd115b7a8cf292a6c8d270697f
Artem <diachenkoartem09@gmail.com>
2022-05-29 22:15:59 +0300

    added top-slider section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,16 @@
-$(function () {
+ $(function(){
+
+    $('.top-slider__inner').slick({
+      dots: true,
+      arrows: false,
+      fade: true,
+      autoplay: true,
+ });
+
+    
   
 });
+
+
+
+/* const { default: gulpfile } = require("../../gulpfile"); */
